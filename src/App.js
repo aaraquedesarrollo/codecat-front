b6644241eb6492ebdbd6e2a6e007d5e1cb671f9f
Raúl Navarro <raul.navarro.uribe@gmail.com>
2021-07-18 11:30:13 +0200

    Añadir contenido html en la página principal

--- src/App.js
+++ src/App.js
@@ -1,5 +1,59 @@
 function App() {
-  return <div>CodeCat</div>;
+  return (
+    <>
+      <header>
+        <ul>
+          <li>
+            <a href="principal">Principal</a>
+          </li>
+          <li>
+            <a href="about">Sobre CodeCat</a>
+          </li>
+        </ul>
+      </header>
+      <main>
+        <img src="src" alt="Dibujo de un gato en píxel art" />
+        <img src="src" alt="Logo de CodeCat" />
+        <button type="button">Login</button>
+        <button type="button">Registro</button>
+      </main>
+      <aside>
+        <article>
+          <h1>Sobre CodeCat</h1>
+          <p>
+            CodeCat es un proyecto que nace de la idea de 4 jóvenes en el
+            proyecto final de un bootcamp de programación.
+          </p>
+          <p>
+            <b>Jose</b> Lorem, ipsum dolor sit amet consectetur adipisicing
+            elit. Culpa pariatur, veniam nisi libero nihil modi debitis nemo vel
+            consequuntur, quas ad. Cum quasi repellendus possimus ipsum, rerum
+            dolorum id. Praesentium!
+          </p>
+          <p>
+            <b>Albert</b> Lorem, ipsum dolor sit amet consectetur adipisicing
+            elit. Culpa pariatur, veniam nisi libero nihil modi debitis nemo vel
+            consequuntur, quas ad. Cum quasi repellendus possimus ipsum, rerum
+            dolorum id. Praesentium!
+          </p>
+          <p>
+            <b>Sandra</b> Lorem, ipsum dolor sit amet consectetur adipisicing
+            elit. Culpa pariatur, veniam nisi libero nihil modi debitis nemo vel
+            consequuntur, quas ad. Cum quasi repellendus possimus ipsum, rerum
+            dolorum id. Praesentium!
+          </p>
+          <p>
+            <b>Raúl</b> Lorem, ipsum dolor sit amet consectetur adipisicing
+            elit. Culpa pariatur, veniam nisi libero nihil modi debitis nemo vel
+            consequuntur, quas ad. Cum quasi repellendus possimus ipsum, rerum
+            dolorum id. Praesentium!
+          </p>
+          <p></p>
+        </article>
+      </aside>
+      <footer>CodeCat &copy; Texto del footer</footer>
+    </>
+  );
 }
 
 export default App;
