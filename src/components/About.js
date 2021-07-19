38f3d1d0853fab4d1fb1ae18e625dc9b20e3a4fa
Raúl Navarro <raul.navarro.uribe@gmail.com>
2021-07-19 12:58:43 +0200

    Estilos pagina principal y funcion bontones login y registro

---
+++ src/components/About.js
@@ -0,0 +1,38 @@
+export const About = () => {
+  return (
+    <aside className="contenedor-secundario row">
+      <article className="col-12" id="about">
+        <h2 className="text-center">Sobre CodeCat</h2>
+        <p>
+          CodeCat es un proyecto que nace de la idea de 4 jóvenes en el proyecto
+          final de un bootcamp de programación.
+        </p>
+        <p>
+          <b>Jose</b> Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Culpa pariatur, veniam nisi libero nihil modi debitis nemo vel
+          consequuntur, quas ad. Cum quasi repellendus possimus ipsum, rerum
+          dolorum id. Praesentium!
+        </p>
+        <p>
+          <b>Albert</b> Lorem, ipsum dolor sit amet consectetur adipisicing
+          elit. Culpa pariatur, veniam nisi libero nihil modi debitis nemo vel
+          consequuntur, quas ad. Cum quasi repellendus possimus ipsum, rerum
+          dolorum id. Praesentium!
+        </p>
+        <p>
+          <b>Sandra</b> Lorem, ipsum dolor sit amet consectetur adipisicing
+          elit. Culpa pariatur, veniam nisi libero nihil modi debitis nemo vel
+          consequuntur, quas ad. Cum quasi repellendus possimus ipsum, rerum
+          dolorum id. Praesentium!
+        </p>
+        <p>
+          <b>Raúl</b> Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Culpa pariatur, veniam nisi libero nihil modi debitis nemo vel
+          consequuntur, quas ad. Cum quasi repellendus possimus ipsum, rerum
+          dolorum id. Praesentium!
+        </p>
+        <p></p>
+      </article>
+    </aside>
+  );
+};
---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <footer className="footer row py-3">
+      <div className="col-12 text-center">CodeCat &copy; Texto del footer</div>
+    </footer>
+  );
+};
