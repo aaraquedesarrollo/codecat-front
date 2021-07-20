789a5633fc37e098c8f69129373bf094bd66c46d
Raúl Navarro <raul.navarro.uribe@gmail.com>
2021-07-20 13:57:41 +0200

    Cabecera con position fixed

--- src/components/Cabecera.js
+++ src/components/Cabecera.js
@@ -1,15 +1,17 @@
 export const Cabecera = () => {
   return (
-    <header className="row">
+    <header className="row fixed-top">
       <nav className="navegador col-12 py-3">
-        <ul className="listado-navegacion list-unstyled row justify-content-around">
-          <li>
-            <a href="#principal">Principal</a>
-          </li>
-          <li>
-            <a href="#about">Sobre CodeCat</a>
-          </li>
-        </ul>
+        <div className="container">
+          <ul className="listado-navegacion list-unstyled row justify-content-around">
+            <li>
+              <a href="#principal">Principal</a>
+            </li>
+            <li>
+              <a href="#about">Sobre CodeCat</a>
+            </li>
+          </ul>
+        </div>
       </nav>
     </header>
   );
