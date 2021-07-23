b85603945da71a2da1ae30c1251678f7b94b1bb5
Albert Araque <aaraquedesarrollo@gmail.com>
2021-07-23 12:31:12 +0200

    Popup que pregunta por el nombre del gato

--- src/components/CodeCat/Habitacion.js
+++ src/components/CodeCat/Habitacion.js
@@ -1,32 +1,90 @@
 import { PropTypes } from "prop-types";
+import { useEffect } from "react";
+import { useState } from "react";
 import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
 import { EjerciciosContext } from "../../context/EjerciciosContext";
+import { GeneralContext } from "../../context/GeneralContext";
 import { Ejercicio } from "./Ejercicio";
 
 export const Habitacion = (props) => {
   const { abrirEjercicios } = props;
-  /* const ejercicios = [
-    { numero: 1, nombre: "header", formacion_minima: "1", tema: "html" },
-    { numero: 2, nombre: "main", formacion_minima: "2", tema: "html" },
-    { numero: 3, nombre: "footer", formacion_minima: "2", tema: "html" },
-    { numero: 4, nombre: "paragraph", formacion_minima: "2", tema: "html" },
-    { numero: 5, nombre: "list", formacion_minima: "3", tema: "html" },
-    { numero: 6, nombre: "button", formacion_minima: "3", tema: "html" },
-  ]; */
+  const { token } = useContext(AuthContext);
+  const { datosUsuario, urlApi } = useContext(GeneralContext);
   const { datosFormaciones } = useContext(EjerciciosContext);
+  const [abrirPopUpGato, setAbrirPopUpGato] = useState(false);
+  const [inputNombreGato, setInputNombreGato] = useState("");
+  const [error, setError] = useState("");
+
+  const submitNombreGato = async (e) => {
+    e.preventDefault();
+    try {
+      const resp = await fetch(
+        urlApi + "codecat/nombre-gato/" + inputNombreGato,
+        {
+          method: "PUT",
+          headers: { Authorization: "Bearer " + token },
+        }
+      );
+      const resultado = await resp.json();
+      if (resultado.error) {
+        throw resultado;
+      }
+      setAbrirPopUpGato(false);
+    } catch (err) {
+      setError(err.mensaje);
+    }
+  };
+
+  useEffect(() => {
+    if (datosUsuario.length !== 0) {
+      if (!datosUsuario.usuario?.gato) {
+        setAbrirPopUpGato(true);
+      }
+    }
+  }, [datosUsuario.length, datosUsuario.usuario?.gato]);
 
   return (
     <section className="col-9">
       <div className="habitacion row">
-        <section className="ventana-ejercicios col-12">
-          {abrirEjercicios && (
+        {abrirEjercicios && (
+          <section className="ventana-ejercicios col-12">
             <ul className="listado-ejercicios list-unstyled row">
               {datosFormaciones.map((ejercicio) => (
                 <Ejercicio key={ejercicio._id} ejercicio={ejercicio} />
               ))}
             </ul>
-          )}
-        </section>
+            )
+          </section>
+        )}
+        {abrirPopUpGato && (
+          <div className="ventana-ejercicios col-12">
+            <div className="formulario-nombre-gato">
+              <form noValidate={true} onSubmit={submitNombreGato}>
+                <div className="form-group">
+                  <label htmlFor="nombre-gato">
+                    Ponle un nombre a tu compañero felino:{" "}
+                  </label>
+                  <input
+                    type="text"
+                    id="nombre-gato"
+                    className="form-control"
+                    value={inputNombreGato}
+                    onChange={(e) => setInputNombreGato(e.target.value)}
+                  ></input>
+                </div>
+                <div className="text-center">
+                  <button type="submit" className="btn">
+                    ¡Empezar aventura!
+                  </button>
+                  {error !== "" && (
+                    <span className="d-block mt-2">{error}</span>
+                  )}
+                </div>
+              </form>
+            </div>
+          </div>
+        )}
       </div>
     </section>
   );
