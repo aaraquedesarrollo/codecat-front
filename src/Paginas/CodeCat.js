8ac53a2286234462aa801294603f3974fedb4220
Raúl Navarro <raul.navarro.uribe@gmail.com>
2021-07-23 12:33:33 +0200

    Declarar abrir ejercicios en app para enviarlo a varios comp

--- src/Paginas/CodeCat.js
+++ src/Paginas/CodeCat.js
@@ -1,13 +1,18 @@
 import { CabeceraCodecat } from "../components/CabeceraCodecat";
 import { MainCodeCat } from "../components/MainCodeCat";
 
-export const CodeCat = () => {
+export const CodeCat = (props) => {
+  const { toggleAbrirEjercicios, abrirEjercicios, setAbrirEjercicios } = props;
   return (
     <>
       <div className="contenedor-body container-fluid">
         <CabeceraCodecat />
         <div className="container">
-          <MainCodeCat />
+          <MainCodeCat
+            toggleAbrirEjercicios={toggleAbrirEjercicios}
+            abrirEjercicios={abrirEjercicios}
+            setAbrirEjercicios={setAbrirEjercicios}
+          />
         </div>
       </div>
     </>
